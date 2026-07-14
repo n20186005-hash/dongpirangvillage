91ac9853fe0b45da67f4cbfea1a608a1c7f92d16
n20186005-hash <n20186005@gmail.com>
2026-07-14 22:18:20 +0800

    更新

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+const siteConfig = {
+  name: 'Dongpirang Mural Village',
+  baseUrl: 'https://dongpirangvillage.com',
+  locales: ['zh', 'en', 'ja', 'ko'] as const,
+};
+
+export default siteConfig;
